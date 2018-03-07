90977090035411c7d57aa3120ff6ec9cf4a1549a
María Mercedes Retolaza Reyna <ret16339@uvg.edu.gt>
2018-03-07 14:30:06 -0600

    delete option by xml in controllers

--- server/grails-app/controllers/server/BillController.groovy
+++ server/grails-app/controllers/server/BillController.groovy
@@ -6,7 +6,7 @@ import grails.transaction.Transactional
 @Transactional(readOnly = true)
 class BillController {
 
-    static responseFormats = ['json', 'xml']
+    static responseFormats = ['json']
     static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
 
     def index(Integer max) {
--- server/grails-app/controllers/server/UsersController.groovy
+++ server/grails-app/controllers/server/UsersController.groovy
@@ -6,7 +6,7 @@ import grails.transaction.Transactional
 @Transactional(readOnly = true)
 class UsersController {
 
-    static responseFormats = ['json', 'xml']
+    static responseFormats = ['json']
     static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
 
     def index(Integer max) {
