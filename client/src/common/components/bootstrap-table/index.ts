fdc49667e161460faee9054756f9bd43cca9c62e
María Mercedes Retolaza Reyna <ret16339@uvg.edu.gt>
2018-03-08 10:46:06 -0600

    delete components

--- client/src/common/components/bootstrap-table/index.ts
+++
@@ -1,7 +0,0 @@
-import UserFormatter from './user-formatter';
-
-export const userFormatter = UserFormatter;
-
-import { dateFormatter as DateFormatter } from './date-formatter';
-
-export const dateFormatter = DateFormatter;
