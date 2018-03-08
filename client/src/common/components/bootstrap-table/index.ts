1d8c9c636d28b8e68d51044d923beca899bb1dc9
María Mercedes Retolaza <mretolaza@stsa.info>
2018-03-08 16:53:25 -0600

    delete paths and imports

--- client/src/common/components/bootstrap-table/index.ts
+++ client/src/common/components/bootstrap-table/index.ts
@@ -1,27 +1,3 @@
-import dateFilter from './date-filter';
-
-export const DateFilter = dateFilter;
-
-import vehicleIconFormat from './vehicle-icon-format';
-
-export const VehicleIconFormat = vehicleIconFormat;
-
-import vehicleTagFormat from './vehicle-tag-format';
-
-export const VehicleTagFormat = vehicleTagFormat;
-
-import cellphoneTagFormat from './cellphone-tag-format';
-
-export const CellphoneTagFormat = cellphoneTagFormat;
-
-import cellphoneIconFormat from './cellphone-icon-format';
-
-export const CellphoneIconFormat = cellphoneIconFormat;
-
-import descriptionFormat from './cellphone-description-format';
-
-export const DescriptionFormat = descriptionFormat;
-
 import UserFormatter from './user-formatter';
 
 export const userFormatter = UserFormatter;
