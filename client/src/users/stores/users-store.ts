20019f589a0c06c3037bdd81d3a1825a37da0705
María Mercedes Retolaza <mretolaza@stsa.info>
2018-03-08 16:54:01 -0600

    fix typo errors

--- client/src/users/stores/users-store.ts
+++ client/src/users/stores/users-store.ts
@@ -1,18 +1,18 @@
-import User from '../types/user';
+import User from '../types/users';
 import { observable, action, toJS } from 'mobx';
 import { Https } from '../../common/util/https';
 import Store from '../../common/types/store';
+import { encodeObject } from '../../common/util/encode-object';
 
 const API_URL = 'nose que ira aqui';
 
 export default class UserStore implements Store {
     @observable isFetching: boolean = false;
     @observable users: User[] = [] as User[];
-    @observable user: User = { id: '-1' } as User;
+    @observable user: User = { id: -1 } as User;
     @observable isFetchingUser: boolean = false;
     @observable errors: { [valueKey: string]: string };
     https: Https;
-
 
     constructor(https: Https) {
         this.https = https;
@@ -32,7 +32,7 @@ export default class UserStore implements Store {
         return response.success;
     }
 
-    @action async fetchUser(id: string) {
+    @action async fetchUser(id: number) {
         this.isFetchingUser = true;
         const response = await this.https.get(`${API_URL}?cmd=detail&id=${id}`);
         if (response.success) {
@@ -42,10 +42,10 @@ export default class UserStore implements Store {
         return response.success;
     }
 
-    @action async deleteUser(id: string) {
+    @action async deleteUser(id: number) {
         const response = await this.https.get(`${API_URL}?cmd=delete&id=${id}`);
         if (response.success) {
-            this.users = this.users.filter(user => user.id !== id);
+            this.users = this.users.filter(user => user.userId !== id);
         } else { this.errors = response.errors; }
         return response.success;
     }
@@ -53,11 +53,11 @@ export default class UserStore implements Store {
     @action async updateUser(user: User) {
         const form = new FormData();
         const response = await
-            this.https.post(`${API_URL}?cmd=update&user_id=${user.id}&${encodeObject(user)}`, form);
+            this.https.post(`${API_URL}?cmd=update&user_id=${user.userId}&${encodeObject(user)}`, form);
         if (response.success) {
             const userList = toJS(this.users);
-            const users = userList.map(usr => usr.id === user.id
-                ? { ...usr, ...user, name: `${user.name} ${user.last_name}` }
+            const users = userList.map(usr => usr.userId === user.userId
+                ? { ...usr, ...user, name: `${user.name} ${user.lastName}` }
                 : usr
             );
             this.users = users;
@@ -65,16 +65,14 @@ export default class UserStore implements Store {
         return response.success;
     }
 
-
-
     @action async createUser(user: User): Promise<boolean> {
         const form = new FormData();
         const response = await this.https.post(`${API_URL}?cmd=create&${encodeObject(user)}`, form);
         if (response.success) {
             this.users = this.users.concat({
                 ...user,
-                id: response.id,
-                name: `${user.name} ${user.last_name}`
+                userId: response.userId,
+                name: `${user.name} ${user.lastName}`
             });
         } else {
             if (response.errors.id) {
@@ -86,10 +84,4 @@ export default class UserStore implements Store {
         return response.success;
     }
 
-    encodeObject = (user: User) => {
-        return Object.keys(user).filter(key => key !== 'id').map(key => {
-            return `${encodeURIComponent(key)}=${encodeURIComponent(user[key])}`;
-        }).join('&');
-
-    }
 }
