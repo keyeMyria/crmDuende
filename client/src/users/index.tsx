5f3ff48c9cd119e7ffddd03aaffed15023b2a561
María Mercedes Retolaza Reyna <ret16339@uvg.edu.gt>
2018-03-07 23:40:32 -0600

    add missing components of users

---
+++ client/src/users/index.tsx
@@ -0,0 +1,20 @@
+import UsersManager from './routes/';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import '../common/resources/styles/styles.css';
+
+declare global {
+    interface Window {
+        // tslint:disable-next-line:no-any
+        MountUsersManager: (idElement: string) => any;
+    }
+}
+
+export default UsersManager;
+
+window.MountUsersManager = (idElement: string) => {
+    ReactDOM.render(
+        <UsersManager />,
+        document.getElementById(idElement) as Element
+    );
+};
