d67ec6ca9a8e6f095775b28b64bae941336c68d9
María Mercedes Retolaza <mretolaza@stsa.info>
2018-03-08 16:54:14 -0600

    delete space

--- client/src/users/index.tsx
+++ client/src/users/index.tsx
@@ -2,7 +2,6 @@ import UsersManager from './routes/';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import '../common/resources/styles/styles.css';
-
 declare global {
     interface Window {
         // tslint:disable-next-line:no-any
