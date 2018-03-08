00553596cf1ed8b40184b32fe9c2c99f660c2661
María Mercedes Retolaza Reyna <ret16339@uvg.edu.gt>
2018-03-07 22:39:45 -0600

    add some common components

---
+++ client/src/common/components/buttons/index.tsx
@@ -0,0 +1,5 @@
+import { CancelButton as cancel } from './cancel-button';
+export const CancelButton = cancel;
+
+import { CreateButton as create } from './create-button';
+export const CreateButton = create;
---
+++ client/src/common/components/context-message/icons/index.ts
@@ -0,0 +1,4 @@
+export const error = require('./error.svg') as string;
+export const success = require('./success.svg') as string;
+export const warning = require('./warning.svg') as string;
+export const copy = require('./copy-to-clipboard.svg') as string; 
