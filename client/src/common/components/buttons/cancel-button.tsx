0b1963becad68a3f6707c7911b5e747fbd713a19
María Mercedes Retolaza Reyna <ret16339@uvg.edu.gt>
2018-03-10 09:03:45 -0600

    fix components

--- client/src/common/components/buttons/cancel-button.tsx
+++ client/src/common/components/buttons/cancel-button.tsx
@@ -1,10 +1,9 @@
 import * as React from 'react';
-import { FormattedMessage } from 'react-intl';
 
 export function CancelButton(props: React.HTMLProps<HTMLButtonElement>) {
   return (
     <button {...props} className="btn btn-default">
-      <FormattedMessage id="global.cancel" />
+      Cancelar
     </button>
   );
 }
--- client/src/common/components/labels/not-available.tsx
+++ client/src/common/components/labels/not-available.tsx
@@ -8,7 +8,7 @@ interface NotAvailable {
 export default function NotAvailable(props: NotAvailable) {
     return (
         <div style={{ color: 'gray', display: 'inline-block' }}>
-            <FormattedMessage id={props.messageId || 'global.not_available'} />
+            <FormattedMessage id={props.messageId || 'No hay disponibles'} />
         </div>
     );
 }
