cfde9cbbd20beef98ee6c8b4d48ac2c51e3c93b0
María Mercedes Retolaza Reyna <ret16339@uvg.edu.gt>
2018-03-10 09:30:23 -0600

    fix errors in styles

--- client/src/users/index.tsx
+++ client/src/users/index.tsx
@@ -1,11 +1,5 @@
 import UsersManager from './routes/';
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
 import '../common/resources/styles/styles.css';
 
 export default UsersManager;
 
-ReactDOM.render(
-    <UsersManager />,
-    document.getElementById('root') as HTMLElement
-);
