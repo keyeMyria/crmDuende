1b3d978ae76c2e2b129609433f5ac57808a7197f
María Mercedes Retolaza Reyna <ret16339@uvg.edu.gt>
2018-03-10 20:04:46 -0600

    implement the basic structure of client component

---
+++ client/src/client/types/clients.d.ts
@@ -0,0 +1,16 @@
+export interface Client {
+    clientId: number;
+    name?: string;
+    lastName?: string;
+    userName?: string;
+    birthayDate?: Date;
+    firstBuy?: Date;
+    lastBuy?: Date;
+    email?: string;
+    mobile?: string;
+    phone?: string;
+    address?: string;
+    countryCode?: string;
+    location?: string;
+    picture?: string;
+}
