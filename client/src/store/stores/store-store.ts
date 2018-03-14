46ee47d5c168613a49e1ddaec7d7637fd1e829b8
María Mercedes Retolaza <mretolaza@stsa.info>
2018-03-14 08:02:09 -0600

    fix errors in store component

--- client/src/store/stores/store-store.ts
+++ client/src/store/stores/store-store.ts
@@ -1,15 +1,18 @@
 import 'whatwg-fetch';
 import { observable, action } from 'mobx';
-import AccountPreference, { TrackingScheduleJSON } from '../types/account-preference';
 import { Https } from '../../common/util/https';
+import { Store } from '../types/store';
 
-const ACC_PREFS_URL: string = '/ajax/accountPrefs.php';
+const API_URL: string = 'algo aqui ';
 
-export default class AccountStore {
+const defaultRequest: RequestInit = {
+    method: 'GET',
+    credentials: 'same-origin',
+};
+export default class StoreStore {
     @observable isFetching: boolean = false;
-    @observable account: AccountPreference = {} as AccountPreference;
-    @observable accounts: AccountPreference[] = [] as AccountPreference[];
-    @observable timezone: string = '';
+    @observable store: Store = {} as Store;
+    @observable stores: Store[] = [] as Store[];
     private https: Https;
 
     constructor(https: Https) {
@@ -17,60 +20,41 @@ export default class AccountStore {
     }
 
     @action fetchDataIfNeeded() {
-        if (!this.accounts || this.accounts.length === 0) {
+        if (!this.stores || this.stores.length === 0) {
             this.fetchData();
         }
     }
 
     @action async fetchData(useFetching: boolean = true) {
         if (useFetching) { this.isFetching = true; }
-        const res = await this.https.get(`${ACC_PREFS_URL}?cmd=list`);
+        const res = await this.https.get(`${API_URL}?cmd=list`);
         if (res.success) {
-            this.accounts = res.data;
-            this.timezone = res.timezone;
+            this.stores = res.data;
         }
         if (useFetching) { this.isFetching = false; }
     }
 
-    @action setAccount(account: AccountPreference) {
-        const scheduleJSON = (typeof account.tracking_schedule_json === 'string')
-            ? JSON.parse(account.tracking_schedule_json as string)
-            : account.tracking_schedule_json;
-        const schedule = scheduleJSON ? addIndexToSchedule(scheduleJSON) : null;
-        this.account = { ...account, tracking_schedule_json: schedule };
-    }
-
     @action resetAccounts() {
-        this.account = { id: '-1' } as AccountPreference;
+        this.store = { storeId: -1 } as Store;
     }
 
-    @action async updateAccount(account: AccountPreference): Promise<boolean> {
-        const form = prepareAccount(account);
-        const res = await this.https.post(`${ACC_PREFS_URL}?cmd=update`, form);
-        if (res.success) {
-            this.accounts = this.accounts.map((item: AccountPreference) => (
-                item.id === account.id ? { ...item, ...account } : item
-            ));
-        }
-        return res.success;
+    @action updateStore(shop: Store): Promise<boolean> {
+        return fetch(`${API_URL}?cmd=update&${encodeObject(shop)}`, defaultRequest)
+            .then((response: Response) => response.json()
+            ).then(({ success }: { success: boolean }) => {
+                if (success) {
+                    this.stores = this.stores.map((item: Store) => (
+                        item.storeId === shop.storeId ? { ...item, ...shop } : item
+                    ));
+                }
+                return success;
+            }).catch(() => false);
     }
 }
 
-const prepareAccount = (account: AccountPreference) => {
-    const acc = account;
-    if (acc.tracking_schedule_json) {
-        acc.tracking_schedule_json = removeIndexFromSchedule(acc.tracking_schedule_json);
-    }
-    const keys = Object.keys(acc);
-    const form = new FormData();
-    keys.forEach(key => {
-        if (!!acc[key]) {
-            if (key === 'tracking_schedule_json') {
-                form.append(key, JSON.stringify(acc[key]));
-            } else {
-                form.append(key, acc[key]);
-            }
-        }
-    });
-    return form;
+const encodeObject = (shop: Store) => {
+    const keys = Object.keys(shop).filter(key => !!shop[key]);
+    return keys.map(key => {
+        return `${encodeURIComponent(key)}=${encodeURIComponent(shop[key])}`;
+    }).join('&');
 };
