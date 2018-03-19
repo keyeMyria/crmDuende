31c4ff1f0f37e79329155344c46c2129ce316b70
María Mercedes Retolaza Reyna <ret16339@uvg.edu.gt>
2018-03-19 17:24:10 -0600

    fix typo

--- client/src/common/components/bootstrap-table/user-formatter.tsx
+++ client/src/common/components/bootstrap-table/user-formatter.tsx
@@ -1,6 +1,6 @@
 import User from '../../../users/types/users';
 
 export default function userFormatter(userId: number, row: any, data: { users: User[] }): string {
-    const selectedUser = data.users.find(user => user.userId === userId);
+    const selectedUser = data.users.find(user => user.id === userId);
     return selectedUser ? selectedUser.name! : '';
 }
